b46d6c1a8ca21e8f31e2e40c9d5930d60b76a169
stanley-agwu <stanleyagwu2016@gmail.com>
2026-01-13 21:16:18 +0100

    feat(Regex): Grouping and Capturing - Alternative Matching - Python

---
+++ Grouping and Capturing/alternative_matching.py
@@ -0,0 +1,47 @@
+# Alternative Matching
+
+# Matching single regex out of several regex -- OR -> |
+
+# Character class --> []
+# Group --> ()
+# Anchors --> ^, $
+
+"""
+Alternations, denoted by the | character, match a single item out of several 
+possible items separated by the vertical bar. When used inside a character 
+class, it will match characters; when used inside a group, it will match 
+entire expressions (i.e., everything to the left or everything to the right 
+of the vertical bar). We must use parentheses to limit the use of alternations.
+"""
+
+"""
+For example:
+
+- (Bob|Kevin|Stuart) will match either Bob or Kevin or Stuart.
+- ([a-f]|[A-F]) will match any of the following characters: a, b, c, d, e, f, 
+    A, B, C, D, E, or F.
+"""
+
+"""
+Task
+
+Given a test string, s, write a RegEx that matches s under the following conditions:
+
+- s must start with Mr., Mrs., Ms., Dr. or Er..
+- The rest of the string must contain only one or more English alphabetic letters 
+    (upper and lowercase).
+"""
+
+import re
+
+pattern = r"^(?:Mr\.|Mrs\.|Ms\.|Dr\.|Er\.)[a-zA-Z]+$"
+
+match = re.match(pattern, "Mr.Teacher") is not None
+match2 = re.match(pattern, "Sir.John") is not None
+
+print(str(match))
+print(str(match2))
+
+# NOTE:
+# In Group, \. -> Literal dot (.) E.g (?: Mr\.)
+# In Character class . -> Literal dot (.) E.g [Mr.]
